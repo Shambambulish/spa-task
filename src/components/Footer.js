4d45ab46c41896b79300518f61e239a3f547633a
Noelle <noelle.traff@gmail.com>
2022-10-11 08:35:08 +0300

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <div className='container'>
+                <span>Please sticky footer here</span>
+            </div>
+        </footer>
+    )
+}
